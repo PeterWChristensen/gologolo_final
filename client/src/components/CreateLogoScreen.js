11a1ab2d9f64104a8f43ba2ec81de48b81f5c537
Peter Christensen <peter.christensen@stonybrook.edu>
2020-04-11 17:39:27 -0400

    added logo parameters for background color, border options, padding and margin

--- client/src/components/CreateLogoScreen.js
+++ client/src/components/CreateLogoScreen.js
@@ -7,11 +7,23 @@ const ADD_LOGO = gql`
     mutation AddLogo(
         $text: String!,
         $color: String!,
-        $fontSize: Int!) {
+        $fontSize: Int!
+        $backgroundColor: String!,
+        $borderColor: String!,
+        $borderRadius: Int!,
+        $borderWidth: Int!,
+        $padding: Int!,
+        $margin: Int!) {
         addLogo(
             text: $text,
             color: $color,
-            fontSize: $fontSize) {
+            fontSize: $fontSize,
+            backgroundColor: $backgroundColor,
+            borderColor: $borderColor,
+            borderRadius: $borderRadius,
+            borderWidth: $borderWidth,
+            padding: $padding,
+            margin: $margin) {
             _id
         }
     }
@@ -20,7 +32,7 @@ const ADD_LOGO = gql`
 class CreateLogoScreen extends Component {
 
     render() {
-        let text, color, fontSize;
+        let text, color, fontSize, backgroundColor, borderColor, borderRadius, borderWidth, padding, margin;
         return (
             <Mutation mutation={ADD_LOGO} onCompleted={() => this.props.history.push('/')}>
                 {(addLogo, { loading, error }) => (
@@ -35,28 +47,73 @@ class CreateLogoScreen extends Component {
                             <div className="panel-body">
                                 <form onSubmit={e => {
                                     e.preventDefault();
-                                    addLogo({ variables: { text: text.value, color: color.value, fontSize: parseInt(fontSize.value) } });
+                                    addLogo({ variables: { text: text.value, color: color.value, fontSize: parseInt(fontSize.value),
+                                                           backgroundColor: backgroundColor.value, borderColor: borderColor.value, 
+                                                           borderRadius: parseInt(borderRadius.value), borderWidth: parseInt(borderWidth.value),
+                                                           padding: parseInt(padding.value), margin: parseInt(margin.value) } });
                                     text.value = "";
                                     color.value = "";
                                     fontSize.value = "";
+                                    backgroundColor.value = "";
+                                    borderColor.value = "";
+                                    borderRadius.value = "";
+                                    borderWidth.value = "";
+                                    padding.value = "";
+                                    margin.value = "";
                                 }}>
                                     <div className="form-group">
                                         <label htmlFor="text">Text:</label>
                                         <input type="text" className="form-control" name="text" ref={node => {
                                             text = node;
-                                        }} placeholder="Text" />
+                                        }} placeholder="Text" defaultValue={"GoLogoLo"}/>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="color">Color:</label>
                                         <input type="color" className="form-control" name="color" ref={node => {
                                             color = node;
-                                        }} placeholder="Color" />
+                                        }} placeholder="Color" defaultValue={"#000000"}/>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="fontSize">Font Size:</label>
                                         <input type="number" className="form-control" name="fontSize" ref={node => {
                                             fontSize = node;
-                                        }} placeholder="Font Size" />
+                                        }} placeholder="Font Size" defaultValue={30}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="backgroundColor">Background Color:</label>
+                                        <input type="color" className="form-control" name="backgroundColor" ref={node => {
+                                            backgroundColor = node;
+                                        }} placeholder="Background Color" defaultValue={"#ffffff"} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="backgroundColor">Border Color:</label>
+                                        <input type="color" className="form-control" name="borderColor" ref={node => {
+                                            borderColor = node;
+                                        }} placeholder="Border Color" defaultValue={"#000000"} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="borderRadius">Border Radius:</label>
+                                        <input type="number" className="form-control" name="borderRadius" ref={node => {
+                                            borderRadius = node;
+                                        }} placeholder="Border Radius" defaultValue={50} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="borderWidth">Border Width:</label>
+                                        <input type="number" className="form-control" name="borderWidth" ref={node => {
+                                            borderWidth = node;
+                                        }} placeholder="Border Width" defaultValue={15} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="padding">Padding:</label>
+                                        <input type="number" className="form-control" name="padding" ref={node => {
+                                            padding = node;
+                                        }} placeholder="Padding" defaultValue={15}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="margin">Margin:</label>
+                                        <input type="number" className="form-control" name="margin" ref={node => {
+                                            margin = node;
+                                        }} placeholder="Margin" defaultValue={10} />
                                     </div>
                                     <button type="submit" className="btn btn-success">Submit</button>
                                 </form>
